81f4e9a5c64f7660fe9b7741b8ff58ea6b18916e
wemi3 <wemilywu3@gmail.com>
2025-05-19 18:04:58 -0500

    change port

--- backend/main.py
+++ backend/main.py
@@ -10,11 +10,11 @@ app.include_router(fantasy.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://vdc-fantasy.vercel.app"],
+    allow_origins=["https://vdc-fantasy.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=True)
