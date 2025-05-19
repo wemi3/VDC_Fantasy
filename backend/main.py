ed8d3cbe140d563ca1800239b391b5582f3747bf
wemi3 <wemilywu3@gmail.com>
2025-05-19 17:45:40 -0500

    add vercel cors

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app.include_router(fantasy.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://vdc-fantasy.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
